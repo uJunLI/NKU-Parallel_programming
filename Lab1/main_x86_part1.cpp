83d2eb9a09464ceb0f1bdcc1ecf1a9b992511471
李纡君git <1198320640@qq.com>
2024-03-22 11:00:32 +0800

    0

--- Lab1/main_x86_part1.cpp
+++ Lab1/main_x86_part1.cpp
@@ -7,16 +7,16 @@ const int N=10000;
 
 double b[N][N],a[N],sum[N];
 
-//��ʼ��
+//初始化
 void init(int n)
 {
     for(int i=0; i<N; i++)
     {
         for(int j=0; j<N; j++)
         {
-            b[i][j] = i+j;//��ʼ������
+            b[i][j] = i+j;//初始化矩阵
         }
-        a[i] = i;//��ʼ������
+        a[i] = i;//初始化向量
     }
 }
 
@@ -70,11 +70,10 @@ void f_unrolled(int n)
     }
 }
 
-
 int main()
 {
     int n,step=100;
-    long long counter;// ��¼����
+    long long counter;// 记录次数
     double seconds ;
     long long head,tail,freq,noww;
     init(N);
@@ -82,7 +81,7 @@ int main()
     for(int n=0;n<=10000;n+=step)
     {
         QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
-        QueryPerformanceCounter((LARGE_INTEGER *)&head);//��ʼ��ʱ
+        QueryPerformanceCounter((LARGE_INTEGER *)&head);//开始计时
 
         counter=0;
         while(true)
@@ -90,14 +89,14 @@ int main()
             QueryPerformanceCounter((LARGE_INTEGER *)&noww);
             if( (noww-head)*1000.0/freq > 10)
                 break;
-            // f_ordinary(n);//ִ�к���
+            // f_ordinary(n);//执行函数
             f_pro(n);
             counter++;
         }
-        QueryPerformanceCounter((LARGE_INTEGER *)&tail );//������ʱ
-        seconds = (tail - head) * 1000.0 / freq ;//��λ ms
+        QueryPerformanceCounter((LARGE_INTEGER *)&tail );//结束计时
+        seconds = (tail - head) * 1000.0 / freq ;//单位 ms
 
-        //������
+        //输出结果
         cout << n <<' '<< counter <<' '<< seconds<<' '<< seconds / counter << endl ;
        if(n==1000)
           step=1000;
